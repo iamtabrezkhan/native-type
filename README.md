d5a9737e5635390c1e2bc521460bd63e0cdb9fb1
Tabrez Khan <khan.t.29@gmail.com>
2020-06-11 15:44:25 +0530

    Update: Readme; Add: more test cases

--- README.md
+++ README.md
@@ -43,6 +43,39 @@ getNativeType(new Set()); // ==> 'set'
 getNativeType(new Promise(() => {})); // ==> 'promise'
 getNativeType(null); // ==> 'null'
 getNativeType(9n); // ==> 'bigint'
+getNativeType(Symbol) // ==> function
+getNativeType(() => {}), // ==> "function"
+getNativeType(Symbol.iterator // ==> symbol
+getNativeType(Symbol("foo")) // ==> symbol
+function* g(i) {
+  yield i;
+}
+getNativeType(g) // ==> generatorfunction
+const it = g();
+getNativeType(it) // ==> generator
+getNativeType(true) // ==> boolean
+getNativeType(new ArrayBuffer()) // ==> arraybuffer
+getNativeType(Object.create(null)) // ==> object
+getNativeType(new RegExp()) // ==> regexp
+getNativeType(/abc/) // ==> regexp
+getNativeType(new WeakSet()) // ==> weakset
+getNativeType(new WeakMap()) // ==> weakmap
+getNativeType(new Error()) // ==> error
+getNativeType(new TypeError()) // ==> error
+getNativeType(arguments) // ==> arguments
+getNativeType(new Int8Array()) // ==> int8array
+getNativeType(new Uint8Array()) // ==> uint8array
+getNativeType(new Uint8ClampedArray()) // ==> uint8clampedarray
+getNativeType(new Int16Array()) // ==> int16array
+getNativeType(new Uint16Array()) // ==> uint16array
+getNativeType(new Int32Array()) // ==> int32array
+getNativeType(new Uint32Array()) // ==> uint32array
+getNativeType(new Float32Array()) // ==> float32array
+getNativeType(new Float64Array()) // ==> float64array
+getNativeType([1, 2, 3][Symbol.iterator]()) // ==> arrayiterator
+getNativeType(new Map()[Symbol.iterator]()) // ==> mapiterator
+getNativeType(new Set()[Symbol.iterator]()) // ==> setiterator
+getNativeType("abc"[Symbol.iterator]()) // ==> stringiterator
 ```
 
 ## Run tests
--- test/index.js
+++ test/index.js
@@ -14,5 +14,41 @@ describe("native-type", () => {
     assert.equal(getNativeType(new Promise(() => {})), "promise");
     assert.equal(getNativeType(null), "null");
     assert.equal(getNativeType(9n), "bigint");
+    assert.equal(getNativeType(Symbol), "function");
+    assert.equal(
+      getNativeType(() => {}),
+      "function"
+    );
+    assert.equal(getNativeType(Symbol.iterator), "symbol");
+    assert.equal(getNativeType(Symbol("foo")), "symbol");
+    function* g(i) {
+      yield i;
+    }
+    assert.equal(getNativeType(g), "generatorfunction");
+    const it = g();
+    assert.equal(getNativeType(it), "generator");
+    assert.equal(getNativeType(true), "boolean");
+    assert.equal(getNativeType(new ArrayBuffer()), "arraybuffer");
+    assert.equal(getNativeType(Object.create(null)), "object");
+    assert.equal(getNativeType(new RegExp()), "regexp");
+    assert.equal(getNativeType(/abc/), "regexp");
+    assert.equal(getNativeType(new WeakSet()), "weakset");
+    assert.equal(getNativeType(new WeakMap()), "weakmap");
+    assert.equal(getNativeType(new Error()), "error");
+    assert.equal(getNativeType(new TypeError()), "error");
+    assert.equal(getNativeType(arguments), "arguments");
+    assert.equal(getNativeType(new Int8Array()), "int8array");
+    assert.equal(getNativeType(new Uint8Array()), "uint8array");
+    assert.equal(getNativeType(new Uint8ClampedArray()), "uint8clampedarray");
+    assert.equal(getNativeType(new Int16Array()), "int16array");
+    assert.equal(getNativeType(new Uint16Array()), "uint16array");
+    assert.equal(getNativeType(new Int32Array()), "int32array");
+    assert.equal(getNativeType(new Uint32Array()), "uint32array");
+    assert.equal(getNativeType(new Float32Array()), "float32array");
+    assert.equal(getNativeType(new Float64Array()), "float64array");
+    assert.equal(getNativeType([1, 2, 3][Symbol.iterator]()), "arrayiterator");
+    assert.equal(getNativeType(new Map()[Symbol.iterator]()), "mapiterator");
+    assert.equal(getNativeType(new Set()[Symbol.iterator]()), "setiterator");
+    assert.equal(getNativeType("abc"[Symbol.iterator]()), "stringiterator");
   });
 });
